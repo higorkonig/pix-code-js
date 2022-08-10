dd89090b166fc0da3671225265178647adb803f0
Higor Konig <higor.konig@gmail.com>
2022-08-10 00:40:31 -0300

    novo build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,15 @@
-declare class Pix {
+export declare class Pix {
     private chavePix;
     private descricao;
     private nomeRecebedor;
     private cidadeRecebedor;
     private codigoTransferencia;
     private valor;
-    constructor(chavePix: string, nomeRecebedor: string, cidadeRecebedor: string, valor: number, descricao?: string, codigoTransferencia?: string);
+    constructor(chavePix: string, nomeRecebedor: string, cidadeRecebedor: string, valor?: number, descricao?: string, codigoTransferencia?: string);
     private _getIdETamanhoEvalor;
     private _montarInfomacoesDoRecebedor;
     private _getAdicionalInformacao;
     generateCode(): string;
+    generateQRCode(): Promise<string>;
     private _getCRC16;
 }
-export default Pix;
